ee9fa61327c2e17bf1aa01aa6229d713748c6be0
Rui Fernando <rui.fernando@aluno.uepb.edu.br>
2026-09-19 01:33:54 -0300

    feat: implementa deleção de usuários

--- src/main/java/com/educandoweb/workshopmongo/services/UserService.java
+++ src/main/java/com/educandoweb/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.educandoweb.workshopmongo.services;
 
 import com.educandoweb.workshopmongo.domain.User;
+import com.educandoweb.workshopmongo.dto.UserDTO;
 import com.educandoweb.workshopmongo.repositories.UserRepository;
 import com.educandoweb.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,18 @@ public class UserService {
     public User findById(String id) {
         return repo.findById(id)
                 .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public void delete(String id){
+        findById(id);
+        repo.deleteById(id);
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
