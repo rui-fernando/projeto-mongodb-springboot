3fc42e88cf8ee87a182cc09c85738f939a7fc9fd
Rui Fernando <rui.fernando@aluno.uepb.edu.br>
2026-09-19 15:37:03 -0300

    feat: implementa busca de posts por ID

---
+++ src/main/java/com/educandoweb/workshopmongo/resources/PostResource.java
@@ -0,0 +1,31 @@
+package com.educandoweb.workshopmongo.resources;
+
+import com.educandoweb.workshopmongo.domain.Post;
+import com.educandoweb.workshopmongo.domain.User;
+import com.educandoweb.workshopmongo.dto.UserDTO;
+import com.educandoweb.workshopmongo.repositories.UserRepository;
+import com.educandoweb.workshopmongo.services.PostService;
+import com.educandoweb.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id){
+
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/com/educandoweb/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.educandoweb.workshopmongo.services;
+
+import com.educandoweb.workshopmongo.domain.Post;
+import com.educandoweb.workshopmongo.domain.User;
+import com.educandoweb.workshopmongo.dto.UserDTO;
+import com.educandoweb.workshopmongo.repositories.PostRepository;
+import com.educandoweb.workshopmongo.repositories.UserRepository;
+import com.educandoweb.workshopmongo.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
