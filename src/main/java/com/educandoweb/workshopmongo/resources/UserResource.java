683ce4e4c7772e6d4bfa9853f1621a4f44a27f13
Rui Fernando <rui.fernando@aluno.uepb.edu.br>
2026-09-19 01:59:45 -0300

    feat: implementa edição de usuário

--- src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
+++ src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
@@ -60,6 +60,16 @@ public class UserResource {
         return ResponseEntity.noContent().build();
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDTO, @PathVariable String id){
+
+        User obj = service.fromDTO(objDTO);
+        obj.setId(id);
+        obj = service.update(obj);
+
+        return ResponseEntity.noContent().build();
+    }
+
     @GetMapping("/debug")
     public String debug() {
         return "Count: " + repo.count();
